0d6ef534f6cc49771d69bc551a00e8146ca40322
q <167793812+0vp@users.noreply.github.com>
2026-09-19 03:05:25 -0400

    Dryft experiment: graph_verify

--- dryft/engine/options.py
+++ dryft/engine/options.py
@@ -72,4 +72,4 @@ VARIANTS = {
 VARIANTS['graph_tuned'] = replace(VARIANTS['graph_fused'], norm_warps=4)
 VARIANTS['graph_verify'] = replace(VARIANTS['graph_fused'], graph_verification=True)
 VARIANTS['graph_rope'] = replace(VARIANTS['graph_fused'], norm_rope=True)
-ACTIVE = 'graph_tuned'
+ACTIVE = 'graph_verify'
