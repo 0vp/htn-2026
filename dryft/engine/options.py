2ceb5d859985ce2b95d2b142ae808900d833ee50
q <167793812+0vp@users.noreply.github.com>
2026-09-19 02:26:48 -0400

    Dryft experiment: graph_folded

--- dryft/engine/options.py
+++ dryft/engine/options.py
@@ -58,4 +58,4 @@ VARIANTS = {
     'combined': Options(norms='all', direct=True, gqa=True, static=True,
                         graph=True, swiglu=True, packed=True),
 }
-ACTIVE = 'graph_fused'
+ACTIVE = 'graph_folded'
